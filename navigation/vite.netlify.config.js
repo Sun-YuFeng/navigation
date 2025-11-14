a2e073d7139d93c9f4e31d42a7b5e9fee406bb43
孙宇枫 <2946949540@qq.com>
2025-11-14 19:10:29 +0800

    修好

--- navigation/vite.netlify.config.js
+++ navigation/vite.netlify.config.js
@@ -1,9 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // 专门用于Netlify部署的配置
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue': 'vue/dist/vue.esm-bundler.js'
+    },
+  },
   build: {
     // 确保Netlify兼容性
     target: 'es2015',
